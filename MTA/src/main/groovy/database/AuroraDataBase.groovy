c61a9ce34ddde4d4498c8f31233b4b7cfec3c5a1
Daniela Domnici <daniela.domnici@esure.com>
2018-12-24 11:27:31 +0000

    MTAQ-1113 -> Create a DataBase parent class

--- MTA/src/main/groovy/database/AuroraDataBase.groovy
+++ MTA/src/main/groovy/database/AuroraDataBase.groovy
@@ -1,64 +1,42 @@
 package database
 
 import groovy.sql.Sql
+import utils.TestDataUtils
+import utils.Utils
 
-import java.sql.SQLException
+class AuroraDataBase extends DataBase {
 
-class AuroraDataBase {
-
-    String DB_HOSTNAME_DEV = "awie-rdspg-dev-motor-01.cjjj0qqeowok.eu-west-1.rds.amazonaws.com"
-    String DB_USERNAME_DEV = "api_jva_motor_mta_deve"
-    String DB_PASSWORD_DEV = "UQfGo34dwEK85dvf"
-    String DB_NAME_DEVE = "deve"
-
-    String DB_HOSTNAME_TEST = "awie-rdspg-test-motor-01.c9jas8kzsysw.eu-west-1.rds.amazonaws.com"
-    String DB_USERNAME_TEST = "api_jva_motor_mta_tste"
-    String DB_PASSWORD_TEST = "wKIpnLKNfdBGc47d"
-    String DB_NAME_TSTE = "tste"
-
-    String DB_PORT = "5432"
-    String DRIVER = "org.postgresql.Driver"
+    static final String environment = Utils.getSystemEnvironment()
 
     String getJdbcUrl() {
         try {
-            String environment = System.getProperty("branch")
             switch (environment) {
                 case "deve13":
-                    return  "jdbc:postgresql://" + DB_HOSTNAME_DEV + ":" + DB_PORT + "/" +  DB_NAME_DEVE
+                    return  "jdbc:postgresql://" + TestDataUtils.AuroraDatabase.DB_HOSTNAME_DEV + ":" + TestDataUtils.AuroraDatabase.DB_PORT + "/" +  TestDataUtils.AuroraDatabase.DB_NAME_DEVE
                     break
                 case "tste13":
-                    return  "jdbc:postgresql://" + DB_HOSTNAME_TEST + ":" + DB_PORT + "/" +  DB_NAME_TSTE
+                    return  "jdbc:postgresql://" + TestDataUtils.AuroraDatabase.DB_HOSTNAME_TEST + ":" + TestDataUtils.AuroraDatabase.DB_PORT + "/" +  TestDataUtils.AuroraDatabase.DB_NAME_TSTE
                     break
+                default:
+                    throw new Exception("Invalid environment" + environment)
                 }
             } catch (Exception e) {
             return null
         }
     }
 
-    Sql setupConnection() {
-        Sql sql = null
-        try {
-            String environment = System.getProperty("branch")
-            switch (environment) {
-                case "deve13":
-                    sql = Sql.newInstance(getJdbcUrl(), DB_USERNAME_DEV, DB_PASSWORD_DEV, DRIVER)
-                    break
-                case "tste13":
-                    sql = Sql.newInstance(getJdbcUrl(), DB_USERNAME_TEST, DB_PASSWORD_TEST, DRIVER)
-                    break
-            }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace()
-        } catch (SQLException e) {
-            e.printStackTrace()
+    @Override
+    Sql setupDataBaseConnection() {
+        switch (environment) {
+            case "deve13":
+                return Sql.newInstance(getJdbcUrl(), TestDataUtils.AuroraDatabase.DB_USERNAME_DEV,
+                        TestDataUtils.AuroraDatabase.DB_PASSWORD_DEV, TestDataUtils.AuroraDatabase.DRIVER)
+            case "tste13":
+                return Sql.newInstance(getJdbcUrl(), TestDataUtils.AuroraDatabase.DB_USERNAME_TEST,
+                        TestDataUtils.AuroraDatabase.DB_PASSWORD_TEST, TestDataUtils.AuroraDatabase.DRIVER)
+            default:
+                throw new Exception("Invalid environment" + environment)
         }
-        return sql
-    }
-
-    def getFirstResult(String query) {
-        Sql sql = setupConnection()
-        def row = sql.firstRow(query)
-        return row
     }
 
     String getMtaData() {
